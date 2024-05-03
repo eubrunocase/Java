55c2441dc1ee1252a5316ab67d83cf20caefc281
Bruno Casé <bruno.case45@gmail.com>
2024-05-03 17:16:16 -0300

    Array 0.1

---
+++ Curso/src/ArraysCollection/Arrays.java
@@ -0,0 +1,16 @@
+package ArraysCollection;
+
+public class Arrays {
+
+    public static void main(String[] args) {
+
+        Double[] a1 = new Double[10];
+
+        a1[2] = 500.0;
+
+        System.out.println(a1[2]);
+
+
+
+    }
+}
