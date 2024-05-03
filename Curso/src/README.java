f764917e3227269cccb033a59bed4ba6ec8c77bf
Bruno Casé <bruno.case45@gmail.com>
2024-05-02 22:28:22 -0300

    readme

---
+++ Curso/src/README.java
@@ -0,0 +1,23 @@
+public @interface README {
+
+    /*
+    * @Author: Bruno Casé
+    * Github: @eubrunocase
+    * Since: 01/2024
+    *
+    *
+    *
+    * Projeto feito por Bruno Dantas de Olveira Casé para estudos da
+    * linguagem de programação Java;
+    *
+    * Nesse projeto você encontrará toda a minha trajetória de aprendizado do ecossistema Java, com auxilio do curso
+    * da Udemy "Java COMPLETO: Do Zero ao Profissional + Projetos!" do professor Eduardo Moura Leitão.
+    *
+    * Iniciando com os fundamentos básicos da linguagem, até a implementação de projetos mais complexos.
+    *
+    * O repositório será atualizado regularmente conforme novos conteúdos forem adicionados.
+    *
+    *
+    * */
+
+}
