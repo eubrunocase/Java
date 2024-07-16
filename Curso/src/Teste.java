d28ed44787c77104d5e7159cccdc7279649b74dd
Bruno Casé <bruno.case45@gmail.com>
2024-07-15 23:49:43 -0300

    tst

--- Curso/src/Teste.java
+++ Curso/src/Teste.java
@@ -5,20 +5,11 @@ public class Teste {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        System.out.println("Digite o primeiro valor booleano:");
-        boolean a = sc.nextBoolean();
-        System.out.println("Digite o segundo valor booleano:");
-        boolean b = sc.nextBoolean();
-        System.out.println("Digite o terceiro valor booleano:");
-        boolean c = sc.nextBoolean();
+        System.out.println("Informe a sua idade: ");
+        int idade = sc.nextInt();
 
-        System.out.println("a AND b: " + (a && b));
-        System.out.println("a OR b: " + (a || b));
-        System.out.println("NOT a: " + (!a));
-
-
-
-
+        String resultado = (idade >= 18) ? "Maior de idade" : "Menor de idade";
+        System.out.println(resultado);
 
     }
 }
