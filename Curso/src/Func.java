a7e7d21f92c5a037f961df844839fb55de96b89c
Bruno Casé <bruno.case45@gmail.com>
2024-07-25 11:50:24 -0300

    fibonacci

--- Curso/src/Func.java
+++ Curso/src/Func.java
@@ -1,2 +1,48 @@
-package PACKAGE_NAME;public class Func {
+
+public class Func {
+
+    public int soma (int a, int b) {
+               return a + b;
+        }
+
+        public Object descricao (String nome, int idade) {
+               return "Nome: " + nome + ", idade " + idade;
+        }
+
+        public int factory (int a) {
+            if (a == 0) {
+                return 1;
+            }
+            return a * factory(a - 1);
+        }
+
+        public int fibonacci (int a) {
+            if (a <= 1) {
+                return a;
+            } return fibonacci(a - 1) + fibonacci(a - 2);
+        }
+
+        public boolean primo (int a) {
+            if (a <= 1) {
+                return false;
+            }
+            for (int i = 2; 1 <= Math.sqrt(a); i++) {
+                if (a % i == 0) {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+
+        public boolean teste (int a) {
+         if (a / 1 == a || a / a == 1) {
+                 return true;
+          }  return false;
+        }
+
+
+
+
+
 }
--- Curso/src/teste.java
+++ Curso/src/teste.java
@@ -1,2 +1,28 @@
-package PACKAGE_NAME;public class teste {
+import java.util.Scanner;
+
+public class teste {
+
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+         Func obj = new Func();
+
+       Object rest0 = obj.soma(500, 500 );
+        System.out.println(rest0);
+
+        Object rest1 = obj.descricao("Bruno", 21);
+         System.out.println(rest1);
+
+        Object rest2 = obj.factory(10);
+         System.out.println(rest2);
+
+        Object rest3 = obj.fibonacci(10);
+         System.out.println(rest3);
+
+        Object rest4 = obj.primo(7);
+        System.out.println(rest4);
+
+        Object rest5 = obj.teste(6);
+        System.out.println(rest5);
+
+    }
 }
