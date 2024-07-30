f0454b0c0fff6438f5bb0ba0b152310f7a012eca
Bruno Casé <bruno.case45@gmail.com>
2024-07-30 16:50:11 -0300

    set

---
+++ Curso/src/Collections/SetNaoOrdenado.java
@@ -0,0 +1,38 @@
+package Collections;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class SetNaoOrdenado {
+    
+    @SuppressWarnings({"rawtypes", "unchecked"})
+    public static void main(String[] args) {
+        HashSet conjunto = new HashSet();
+
+        conjunto.add(1.0);
+        conjunto.add(10);
+        conjunto.add(true);
+        conjunto.add("Bruno");
+
+        System.out.println(conjunto.size());
+
+        conjunto.remove(10);
+        conjunto.remove(1.0);
+        System.out.println(conjunto.size());
+
+        Set conjunto2 = new HashSet();
+
+        conjunto2.add(5.0);
+        conjunto2.add("Zaira");
+        conjunto2.add(false);
+        System.out.println(conjunto2.size());
+
+        conjunto.addAll(conjunto2);
+        System.out.println(conjunto);
+
+        conjunto.clear();
+        System.out.println(conjunto);
+
+
+    }
+}
